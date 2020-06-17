24be80223ea119c1020994e2dd825582922e9d3f
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-06-17 21:29:31 +0300

    Added example scripts.

--- example/time_varying_coupling.jl
+++ example/time_varying_coupling.jl
@@ -4,19 +4,24 @@
 using DynamicalNetworks 
 using Plots 
 
-#  Construct the netmodel 
+# Construct the netmodel 
 n = 4 
 d = 3 
-ϵ = 10.
-pcm = PCM(high=ϵ, low=0.01ϵ, period=50.) 
+T = 200. 
+ti = 0. 
+dt = 0.01 
+tf = 1000.
+ε = 10.
+pcm = PCM(high=ε, low=0.01ε, period=T) 
+
 E = [
-    t -> -3*pcm(t) t -> 3*pcm(t) t -> -1*pcm(t) t -> 1*pcm(t);
-    t -> 3*pcm(t) t -> -3*pcm(t) t -> 1*pcm(t) t -> -1*pcm(t);
-    t -> -1*pcm(t) t -> 1*pcm(t) t -> -3*pcm(t) t -> 3*pcm(t);
-    t -> 1*pcm(t) t -> -1*pcm(t) t -> 3*pcm(t) t -> -3*pcm(t)
+    t -> -3*pcm(t)  t -> 3*pcm(t)       t -> -ε     t -> ε;
+    t -> 3*pcm(t)   t -> -3*pcm(t)      t -> ε      t -> -ε;
+    t -> -ε         t -> ε              t -> -3ε    t -> 3ε;
+    t -> ε          t -> -ε             t -> 3ε     t -> -3ε
     ]
 P = coupling(1, 3)
-netmodel = network(ForcedLorenzSystem, E, P)
+netmodel = network(ForcedNoisyLorenzSystem, E, P)
 
 # Add writer to netmodel 
 addnode!(netmodel, Writer(input=Inport(12)), label=:writer)
@@ -26,15 +31,14 @@ addbranch!(netmodel, :node3 => :writer, 1:3 => 7:9)
 addbranch!(netmodel, :node4 => :writer, 1:3 => 10:12)
 
 # Simulate the netmodel 
-sim = simulate!(netmodel, 0., 0.01, 250)
+sim = simulate!(netmodel, ti, dt, tf - dt)
 
 # Read the simulation data 
 t, x = read(getnode(netmodel, :writer).component)
 
 # Plot the results 
-plot(layout=(2,2))
-plot!(t, x[:, 1], subplot=1)
-plot!(x[:, 1] - x[:, 4], subplot=2)
-plot!(x[:, 4] - x[:, 7], subplot=3)
-plot!(x[:, 7] - x[:, 10], subplot=4)
-
+plot(layout=(2,2), xticks = ti : T : tf)
+plot!(t, x[:, 1],               subplot=1)
+plot!(t, x[:, 1] - x[:, 4],     subplot=2)
+plot!(t, x[:, 4] - x[:, 7],     subplot=3)
+plot!(t, x[:, 7] - x[:, 10],    subplot=4)
