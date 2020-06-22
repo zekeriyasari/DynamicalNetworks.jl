78e337e3ff203adac3d7d97f7e12cf6e13ce1d54
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-06-22 03:01:31 +0300

    Added utility fucntions for data file processing.

--- simulation/process.jl
+++ simulation/process.jl
@@ -1,10 +1,15 @@
 # This file includes the code to process simulation data files to calculate the ber performance of cluster synchronization communication 
 
 using Jusdl 
-
-# TODO: Simulation directory must recorded 
+using JLD2, FileIO
 
 # --------------------------------- Utility functions -------------------------- # 
+
+""" Reads simuation config file """
+read_config(simdir) = load(joinpath(simdir, "config.jld2"))
+
+""" Calculates sample per bits """
+calculate_sample_per_bits(config) = floor(Int, config["bit-duration"] / config["sampling-period"])
 
 """ Process simulation data file """
 function process_file(filepath)
@@ -20,8 +25,9 @@ function process_simulation(snrdir) end
 
 # ----------------------------------- Main ------------------------------------- # 
 
-# Read all simulation data files 
 simdir = "/home/sari/Desktop/Simulation-2020-06-21T16-02-05/"
+
+# Read all simulation data files 
 filetree = walkdir(simdir)
 take!(filetree) # Pop the root directory `test` in which `runtests.jl` is.
 for (snrdir, _, datafiles) in filetree
