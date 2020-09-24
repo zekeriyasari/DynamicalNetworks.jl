77283c07df29e96bf7a7a2737cb6639f0eb7675d
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-09-24 19:31:44 +0300

    Cleaned up

--- example/pcm_waveform.jl
+++
@@ -1,9 +0,0 @@
-# This file includes an example file for PCM waveform 
-
-using DynamicalNetworks
-using Plots 
-
-pcm = PCM(bits=rand(Bool, 10), period=2.)
-ti, dt, tf = 0, 0.01, 10.
-tr = collect(ti : dt : tf)
-plot(tr, pcm.(tr))
--- stash/simulation/ber.jl
+++
@@ -1,9 +0,0 @@
-# This file includes ber calculation 
-
-function ber_exp(expdir)
-    genbits = load(joinpath(expdir, "bits.jld2"))["bits"]
-    extbits = load(joinpath(expdir, "extbits.jld2"))["bits"]
-    length(findall(genbits .== extbits)) / length(genbits) 
-end
-ber_snr(snrdir) = mean(ber_exp.(readdir(snrdir, join=true)))
-ber_sim(simdir) = ber_snr.(filter(isdir, readdir(simdir, join=true)))
