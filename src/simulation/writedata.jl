b08154bd7069f10f8f377267e471da588bbfddd7
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2020-10-01 19:51:34 +0300

    Revised writedata.

--- src/simulation/writedata.jl
+++ src/simulation/writedata.jl
@@ -2,14 +2,12 @@ using JLD2
 
 function writedata(path, sol; filename::String="data.jld2", savenoise::Bool=false)
     isdir(path) || mkpath(path)
-    t, x = sol.t, sol.u
-    @save joinpath(path, filename) sol.t sol.u
-    # jldopen(joinpath(path, filename), "w") do file
-    #     file["sol_t"] = sol.t  
-    #     file["sol_x"] = sol.u
-    #     if savenoise
-    #         file["noise_t"] = sol.W.t
-    #         file["noise_u"] = sol.W.u
-    #     end
-    # end
+    jldopen(joinpath(path, filename), "w") do file
+         file["sol_t"] = sol.t  
+         file["sol_x"] = sol.u
+         if savenoise
+             file["noise_t"] = sol.W.t
+             file["noise_u"] = sol.W.u
+         end
+    end
 end
